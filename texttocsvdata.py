e5e490dfdb5d62251978c06b84b2e22a4314f0fb
officialkzhang <kevin.zhang@ufl.edu>
2023-01-28 16:52:22 -0500

    Edited script file to add function for deciphering JSON object for intent

--- texttocsvdata.py
+++ texttocsvdata.py
@@ -10,6 +10,17 @@ def JSON_to_keywordDict(response) -> dict:
     return keywordDict
 
 
+def JSON_to_intentDict(response) -> dict:
+    intentDict = {"news": [], "query": [], "spam": [], "marketing": [], "feedback": []}
+    for dicts in response["intent"]:
+        if dicts:
+            innerDict = dicts["intent"]
+            for key, value in innerDict.items():
+                intentDict[key].append(value)
+
+    return intentDict
+
+
 def main():
     # API key
     paralleldots.set_api_key("2BD2GHXk4JCZdQHphGuUYZEXXkhoxFRqjbqbRK5M4YA")
@@ -20,10 +31,10 @@ def main():
         "you will be unable to send and recieve emails until this issue has been resolved CLICK HERE TO VALIDATE NOW."
     ]
 
-    # returns a JSON object for keywords in the text
-    response = paralleldots.batch_keywords(text)
+    # returns a JSON object for intent in the text
+    response = paralleldots.batch_intent(text)
 
-    print(JSON_to_keywordDict(response))
+    print(JSON_to_intentDict(response))
 
 
 if __name__ == "__main__":
