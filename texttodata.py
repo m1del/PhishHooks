8d40f08a69a5c57e863d822afb4e17e0e12ba555
Kevin Pham <92887651+KNPham1337@users.noreply.github.com>
2023-01-29 00:13:56 -0500

    Update texttodata.py
    Updated batch functions to deal with only one string

--- texttodata.py
+++ texttodata.py
@@ -3,35 +3,30 @@ import language_tool_python
 import time
 
 def JSON_to_keywordDict(response) -> dict:
+    # list[dict] key = word, val = 
     keywordDict = {}
-    for lists in response["keywords"]:
-        if lists:
-            for dicts in lists:
-                keywordDict[dicts["keyword"]] = dicts["confidence_score"]
+    for dicts in response["keywords"]:
+        keywordDict[dicts["keyword"]] = dicts["confidence_score"]
     return keywordDict
 
 
 def JSON_to_intentDict(response) -> dict:
     intentDict = {"news": [], "query": [], "spam": [], "marketing": [], "feedback": []}
-    for dicts in response["intent"]:
-        if dicts:
-            innerDict = dicts["intent"]
-            for key, value in innerDict.items():
-                intentDict[key].append(value)
+    for key, value in response["intent"].items():
+        intentDict[key].append(value)
 
     return intentDict
 
 
 def JSON_to_emotionDict(response) -> dict:
     emotionDict = {"Fear": [], "Bored": [], "Excited": [], "Angry": [], "Happy": [], "Sad": []}
-    for dicts in response["emotion"]:
-        for key, value in dicts.items():
-            emotionDict[key].append(value)
+    for key, value in response["emotion"].items():
+        emotionDict[key].append(value)
 
     return emotionDict
 
 
-def writeToTXT(text, grammarErrors, keyWordDict, intentDict, emotionDict):
+def writeToTXT(text, grammarErrors, keyWordDict, intentDict):
     with open("data.txt", "a") as f:
         f.write(f"Text: {text}\n")
         f.write(f"Grammar Errors: {grammarErrors}\n")
@@ -42,13 +37,10 @@ def writeToTXT(text, grammarErrors, keyWordDict, intentDict, emotionDict):
         f.write("Intents:\n")
         for key, value in intentDict.items():
             f.write(f"{key}: {value}\n")
-        f.write("Emotions:\n")
-        for key, value in emotionDict.items():
-            f.write(f"{key}: {value}\n")
 
 
 def check_grammar(text) -> int:
-    tool = language_tool_python.LanguageTool('en-US')
+    tool = language_tool_python.LanguageTool('en-US', config={ 'cacheSize': 10000, 'pipelineCaching': True })
     return len(tool.check(text))
 
 def main():
@@ -56,34 +48,28 @@ def main():
     paralleldots.set_api_key("2BD2GHXk4JCZdQHphGuUYZEXXkhoxFRqjbqbRK5M4YA")
 
     # Example text
-    text = [
-        "Hello, Are you currently in the US? Here is an opportunity for you to work part time after classes and earn "
-        "$500 weekly. The job is completely done online and can be completed anytime in the evening/night at home and "
-        "won't take much of your time daily, you don't have to be online all day and don't need any professional "
-        "skill to do the job, all you need is just come online before going to bed to forward all order of the day "
-        "made by agents to the supplier and you are done for the day."]
-
+    text = ["Good day, Please, answer the questions Employee Survey - it won`t take long. Waiting for you to go through this survey ASAP. You can find the survey here: hxxps://docs.google.com/document/d/e/2PACX-1vSjKdTddXL-psR2rYotGSJuwOeBHUKkulbrhy78PHX6VtdJWFurH9mEmeV8PLVm1t4P5W0msKzpCg3N/pub HR Department Analyst Uc Regents;"]
     t0 = time.time()
     errors = check_grammar(" ".join(text))
     t1 = time.time()
     print(f"Checked grammar, took {t1-t0:.2f} seconds")
 
-    response = paralleldots.batch_keywords(text)
+    response = paralleldots.keywords(text)
     keywordDict = JSON_to_keywordDict(response)
     t2 = time.time()
     print(f"Retrieved keywords, took {t2-t1:.2f} seconds")
 
-    response = paralleldots.batch_intent(text)
+    response = paralleldots.intent(text)
     intentDict = JSON_to_intentDict(response)
     t3 = time.time()
     print(f"Retrieved intents, took {t3-t2:.2f} seconds")
 
-    response = paralleldots.batch_emotion(text)
-    emotionDict = JSON_to_emotionDict(response)
-    t4 = time.time()
-    print(f"Retrieved emotions, took {t4-t3:.2f} seconds")
+    #response = paralleldots.emotion(text)
+    #emotionDict = JSON_to_emotionDict(response)
+    #t4 = time.time()
+    #print(f"Retrieved emotions, took {t4-t3:.2f} seconds")
 
-    writeToTXT(" ".join(text), errors, keywordDict, intentDict, emotionDict)
+    writeToTXT(" ".join(text), errors, keywordDict, intentDict)
     print(f"Total time: {(time.time() - t0):.2f} seconds")
 
 
