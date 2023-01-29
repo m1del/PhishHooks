41516ce4f1972011487191e006ca1de2fe7ae4ef
Kevin Zhang <65622423+officialkzhang@users.noreply.github.com>
2023-01-29 01:30:39 -0500

    Combined into one script

--- texttodata.py
+++ texttodata.py
@@ -1,9 +1,13 @@
+import csv
 import paralleldots
-import language_tool_python
 import time
+import language_tool_python
+
+SPAMWORDS_FILENAME = "spamwords.csv"
+AVG_SPAM_COEFF = 0.082
+
 
 def JSON_to_keywordDict(response) -> dict:
-    # list[dict] key = word, val = 
     keywordDict = {}
     for dicts in response["keywords"]:
         keywordDict[dicts["keyword"]] = dicts["confidence_score"]
@@ -11,39 +15,63 @@ def JSON_to_keywordDict(response) -> dict:
 
 
 def JSON_to_intentDict(response) -> dict:
-    intentDict = {"news": [], "query": [], "spam": [], "marketing": [], "feedback": []}
+    intentDict = {"news": 0, "query": 0, "spam": 0, "marketing": 0, "feedback": 0}
     for key, value in response["intent"].items():
-        intentDict[key].append(value)
+        intentDict[key] = value
 
     return intentDict
 
 
-def JSON_to_emotionDict(response) -> dict:
-    emotionDict = {"Fear": [], "Bored": [], "Excited": [], "Angry": [], "Happy": [], "Sad": []}
-    for key, value in response["emotion"].items():
-        emotionDict[key].append(value)
+def import_spam_keywords() -> dict:
+    spam_words_dict = {}
+    with open(SPAMWORDS_FILENAME, "r") as csvfile:
+        reader = csv.reader(csvfile)
 
-    return emotionDict
+        for row in reader:
+            if row:
+                spam_words_dict[row[0]] = row[1]
+
+    return spam_words_dict
 
 
-def writeToTXT(text, grammarErrors, keyWordDict, intentDict):
-    with open("data.txt", "a") as f:
-        f.write(f"Text: {text}\n")
-        f.write(f"Grammar Errors: {grammarErrors}\n")
-        f.write("Keywords: ")
-        for key in keyWordDict:
-            f.write(key + " ")
-        f.write("\n")
-        f.write("Intents:\n")
-        for key, value in intentDict.items():
-            f.write(f"{key}: {value}\n")
+def determine_keyword_coefficient(text, spam_words_dict, keywordDict) -> float:
+    # Split string to a list of words
+    words = text.split()
+    total = 0.0
+
+    # Find the max confidence score of the keywords
+    maxKeyWordConfidence = max(keywordDict.values())
+
+    for word in words:
+        lower_word = word.lower()
+        # If the word is a delineated spam word
+        if lower_word in spam_words_dict:
+            keywordDictConfidence = 0
+            spam_coefficient = float(spam_words_dict[lower_word])
+            # Check each keyword in the keyword dict
+            for key in keywordDict:
+                # If the spam word is a substring of the keyword (which may be a phrase)
+                if lower_word in key.lower():
+                    # Maintain the largest confidence score from the keywords the spam word may be a substring of
+                    keywordDictConfidence = max(keywordDictConfidence, keywordDict[key])
+            multiplier = 1
+            if keywordDictConfidence != 0:
+                # Add to the multiplier of 1 the proportion of the confidence score with the max confidence score
+                multiplier += (keywordDictConfidence / maxKeyWordConfidence)
+
+            total += (spam_coefficient * multiplier)
+
+    keyword_coefficient = total / AVG_SPAM_COEFF / len(words)
+    # 0.0 - 0.20 seems typical of a normal email, >>0.20 seems to indicate phishing emails
+    return keyword_coefficient
 
 
 def check_grammar(text) -> int:
     tool = language_tool_python.LanguageTool('en-US', config={ 'cacheSize': 10000, 'pipelineCaching': True })
     return len(tool.check(text))
 
-def intent_Weights(intentDict) -> int:
+
+def intent_weights(intentDict) -> int:
     # int of 0, 1, 2
     # Check for highest 2 categories, if they are spam + marketing = 2, and on and on
 
@@ -74,34 +102,38 @@ def intent_Weights(intentDict) -> int:
 
     return weight
 
+
 def main():
     # API key
     paralleldots.set_api_key("2BD2GHXk4JCZdQHphGuUYZEXXkhoxFRqjbqbRK5M4YA")
 
-    # Example text
-    text = ["Good day, Please, answer the questions Employee Survey - it won`t take long. Waiting for you to go through this survey ASAP. You can find the survey here: hxxps://docs.google.com/document/d/e/2PACX-1vSjKdTddXL-psR2rYotGSJuwOeBHUKkulbrhy78PHX6VtdJWFurH9mEmeV8PLVm1t4P5W0msKzpCg3N/pub HR Department Analyst Uc Regents;"]
+    text = "Dear Student, Your access to your library account is expiring soon due to inactivity. To continue to have " \
+           "access to the library services, you must reactivate your account. For this purpose, click the web address " \
+           "below or copy and paste it into your web browser. A successful login will activate your account and you " \
+           "will be redirected to your library profile. (link " \
+           "is external) If you are not able to login, please contact at xxxxx@berkeley.edu(link sends e-mail) for " \
+           "immediate assistance. "
+
+    spam_words_dict = import_spam_keywords()
+
     t0 = time.time()
     errors = check_grammar(" ".join(text))
     t1 = time.time()
-    print(f"Checked grammar, took {t1-t0:.2f} seconds")
+    print(f"Checked grammar, took {t1 - t0:.2f} seconds")
 
     response = paralleldots.keywords(text)
     keywordDict = JSON_to_keywordDict(response)
     t2 = time.time()
-    print(f"Retrieved keywords, took {t2-t1:.2f} seconds")
+    print(f"Retrieved keywords, took {t2 - t1:.2f} seconds")
 
     response = paralleldots.intent(text)
     intentDict = JSON_to_intentDict(response)
     t3 = time.time()
-    print(f"Retrieved intents, took {t3-t2:.2f} seconds")
+    print(f"Retrieved intents, took {t3 - t2:.2f} seconds")
 
-    #response = paralleldots.emotion(text)
-    #emotionDict = JSON_to_emotionDict(response)
-    #t4 = time.time()
-    #print(f"Retrieved emotions, took {t4-t3:.2f} seconds")
-
-    writeToTXT(" ".join(text), errors, keywordDict, intentDict)
-    print(f"Total time: {(time.time() - t0):.2f} seconds")
+    print(f"Keyword Coefficient: {determine_keyword_coefficient(text, spam_words_dict, keywordDict)}")
+    print(f"Grammatical Errors: {errors}")
+    print(f"Intent Weights: {intent_weights(intentDict)}")
 
 
 if __name__ == "__main__":
